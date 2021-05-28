1230b6126e9a22c167c7fbb6a455a169609359c1
Annemette <anne7n62@stud.kea.dk>
2021-05-28 10:50:38 +0200

    working on validation and inputmask

--- src/Payment.js
+++ src/Payment.js
@@ -1,181 +1,171 @@
 import { useHistory } from "react-router-dom";
 
-import React from 'react';
+import React from "react";
 import { useState } from "react";
 import { Input } from "antd";
 import InputMask from "react-input-mask";
-import Cards from 'react-credit-cards';
-import 'react-credit-cards/es/styles-compiled.css';
+import Cards from "react-credit-cards";
+import "react-credit-cards/es/styles-compiled.css";
 import "antd/dist/antd.css";
 
 import postData from "./Post";
 import PaymentBasket from "./PaymentBasket";
 
 function Payment(props) {
+  let filteredPostOrders = props.basket.map((order) => {
+    return { name: order.product.name, amount: Number(order.amount) };
+  });
 
-    let filteredPostOrders = props.basket.map(order => {
-        return { name: order.product.name, amount: Number(order.amount) };
-    });
+  let location = useHistory();
 
-    let location = useHistory();
+  console.log(filteredPostOrders);
 
-    // console.log(filteredPostOrders);
+  return (
+    <div className="Payment">
+      <div className="BasketDetails">
+        <h1>Basket details</h1>
+        <PaymentBasket {...props} />
+        <p>Total:</p>
+        <span className="TotalPrice">500 DKK</span>
+      </div>
+      <div className="PaymentDetails">
+        <h1>Payment Details</h1>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            orderSubmit(filteredPostOrders);
+            location.push("/thanks");
+          }}
+        >
+          <PaymentForm />
+          <button type="primary" className="SubmitButton" type="submit">
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
 
-    const totalArr = props.basket.map((beer) => {
-
-        const priceObject = beer.amount * beer.product.price;
-
-        return priceObject;
-    });
-
-    const totalAmount = totalArr.reduce(
-        (previousScore, currentScore, index) => previousScore + currentScore,
-        0);
-
-    function orderSubmit(fullData) {
-        postData(
-            fullData,
-            'https://dreaming-of-foobar.herokuapp.com/order',
-            data => {
-                return data.message === "added" ? (
-                    <div>
-                        {props.setOrderId(data.id)}
-                        {console.log(data.id)}
-                    </div>
-                ) : null;
-            }
-        );
-    }
-
-
-
-    return (
-        <div className="Payment">
-            <div className="BasketDetails">
-                <h1>Basket details</h1>
-                <PaymentBasket {...props} />
-                <p>Total:</p>
-                <span className="TotalPrice">{totalAmount} DKK</span>
-            </div>
-            <div className="PaymentDetails">
-                <h1>Payment Details</h1>
-                <form onSubmit={e => {
-                    e.preventDefault();
-                    orderSubmit(filteredPostOrders);
-                    location.push("/thanks");
-                }}>
-                    <PaymentForm />
-                    <button className="SubmitButton" type="submit">Submit</button>
-                </form>
-            </div>
-        </div>
-    );
+function orderSubmit(fullData) {
+  postData(fullData, "https://dreaming-of-foobar.herokuapp.com/order", (data) => {
+    return data.message === "Order went through" ? <div></div> : <div> error </div>;
+  });
 }
 
 class PaymentForm extends React.Component {
+  state = {
+    cvc: "",
+    expiry: "",
+    focus: "",
+    name: "",
+    number: "",
+    cardnumberValid: true,
+    dateValid: true,
+  };
 
-    state = {
-        cvc: "",
-        expiry: "",
-        focus: "",
-        name: "",
-        number: "",
-    };
+  _renderCard = () => () => {
+    const [cardnumber] = useState("");
 
-    _renderCard = () => () => {
-        const [cardnumber] = useState("");
+    return <div>{cardnumber}</div>;
+  };
 
-        return <div>{cardnumber}</div>
+  _renderName = () => () => {
+    const [name] = useState("");
+
+    return <div>{name}</div>;
+  };
+
+  _renderExpiration = () => () => {
+    const [monthYear] = useState("");
+
+    return <div>{monthYear}</div>;
+  };
+
+  handleInputBlurCC = (e) => {
+    if (e.target.value.length != 19) {
+      this.setState({ cardnumberValid: false });
+    } else {
+      this.setState({ cardnumberValid: true });
     }
+  };
 
-    _renderName = () => () => {
-        const [name] = useState("");
-
-        return <div>{name}</div>
+  handleInputBlurDate = (e) => {
+    if (e.target.value.length != 4) {
+      this.setState({ dateValid: false });
+    } else {
+      this.setState({ dateValid: true });
     }
+  };
 
-    _renderExpiration = () => () => {
-        const [monthYear] = useState("");
+  handleInputFocus = (e) => {
+    this.setState({ focus: e.target.name });
+  };
 
-        return <div>{monthYear}</div>
-    }
+  handleInputChange = (e) => {
+    const { name, value } = e.target;
 
-    handleInputFocus = e => {
-        this.setState({ focus: e.target.name });
-    };
+    this.setState({ [name]: value });
+  };
 
-    handleInputChange = e => {
-        const { name, value } = e.target;
+  render() {
+    return (
+      <div id="PaymentForm">
+        <Cards cvc={this.state.cvc} expiry={this.state.expiry} focused={this.state.focus} name={this.state.name} number={this.state.number} />
+        <div className="form-control">
+          <label htmlFor="name">Name</label>
+          <Input name="name" type="text" placeholder="Enter your full name" required minLength="2" value={this.name} onChange={this.handleInputChange} onFocus={this.handleInputFocus} />
+          <p class="error-message">Please enter your full name</p>
+        </div>
 
-        this.setState({ [name]: value });
-    };
+        <div className="form-control">
+          <label htmlFor="cardnumber">Card number</label>
 
-    render() {
-        return (
-            <div id="PaymentForm">
-                <Cards
-                    cvc={this.state.cvc}
-                    expiry={this.state.expiry}
-                    focused={this.state.focus}
-                    name={this.state.name}
-                    number={this.state.number}
-                />
-                <div className="form-control">
-                    <label htmlFor="name">Name</label>
-                    <Input
-                        name="name"
-                        type="text"
-                        required
-                        minLength="2"
-                        value={this.name}
-                        onChange={this.handleInputChange}
-                        onFocus={this.handleInputFocus}
-                    />
-                </div>
-                <div className="form-control">
-                    <label htmlFor="cardnumber">Card number</label>
-                    <Input name="number"
-                        mask="9999 9999 9999 9999"
-                        value={this.cardnumber}
-                        maskchar=""
-                        minLength="16"
-                        className="ant-input"
-                        onChange={this.handleInputChange}
-                        onFocus={this.handleInputFocus}
-                        required
-                    />
-                </div>
-                <div className="form-control">
-                    <label htmlFor="monthyear">Expiration date</label>
-                    <InputMask
-                        mask="99/99"
-                        maskchar=""
-                        name="expiry"
-                        className="ant-input"
-                        required
-                        value={this.monthYear}
-                        onChange={this.handleInputChange}
-                        onFocus={this.handleInputFocus}
-                        minLength="17"
-                    ></InputMask>
-                </div>
-                <div className="form-control">
-                    <label htmlFor="monthyear">CVC</label>
-                    <InputMask
-                        mask="999"
-                        maskchar=""
-                        name="cvc"
-                        className="ant-input"
-                        required
-                        value={this.cvc}
-                        onChange={this.handleInputChange}
-                        onFocus={this.handleInputFocus}
-                        minLength="17"
-                    ></InputMask>
-                </div>
-            </div >
-        );
-    }
+          <InputMask
+            name="number"
+            id="cardnumber"
+            placeholder="Enter your card no."
+            type="text"
+            required
+            minLength="19"
+            //maxLength="19"
+            mask="9999 9999 9999 9999"
+            value={this.number}
+            maskChar=""
+            className={`ant-input ${this.state.cardnumberValid ? "" : "custom"}`}
+            onBlur={this.handleInputBlurCC}
+            onChange={this.handleInputChange}
+            onFocus={this.handleInputFocus}
+          />
+          <p class="error-message">Card no. needs to be 16 digits</p>
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="monthyear">Expiration date</label>
+          <InputMask
+            id="monthyear"
+            mask="99/99"
+            placeholder="Enter month/year, ex: 12/05"
+            className={`ant-input ${this.state.dateValid ? "" : "custom"}`}
+            maskChar=""
+            name="expiry"
+            className="ant-input"
+            required
+            value={this.monthYear}
+            onBlur={this.handleInputBlurDate}
+            onChange={this.handleInputChange}
+            onFocus={this.handleInputFocus}
+          ></InputMask>
+          <p class="error-message">Please enter mm/yy</p>
+        </div>
+        <div className="form-control">
+          <label htmlFor="monthyear">CVC</label>
+          <Input placeholder="Enter CVC, ex: 232" minLength="3" maxLength="3" maskChar="" name="cvc" className="ant-input" required value={this.cvc} onChange={this.handleInputChange} onFocus={this.handleInputFocus}></Input>
+          <p class="error-message">CVC is the 3 numbers on the back of your card</p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Payment;
