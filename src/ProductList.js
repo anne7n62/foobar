da225377f081c86087641f5b766c5819b6016bd4
Annemette <anne7n62@stud.kea.dk>
2021-06-03 22:55:43 +0200

    Removed animation

--- src/ProductList.js
+++ src/ProductList.js
@@ -1,14 +1,8 @@
-import React, { useEffect } from "react";
-import gsap from "gsap";
-
 import Product from "./Product.js";
 import FilterNav from "./FilterNav.js";
 
 function ProductList({ btnCategories, products, filteredBeers, addToBasket, removeFromBasket, basket, taps, setCategory }) {
-  useEffect(() => {
-    gsap.fromTo(".product_list", { opacity: 0.2 }, { opacity: 1, duration: 3 });
-  });
-
+ 
   const productlist = filteredBeers.map((tap) => <Product className="product" tap={tap} filteredBeers={filteredBeers} basket={basket} products={products} key={tap.name} addToBasket={addToBasket} removeFromBasket={removeFromBasket}></Product>);
 
   return (
