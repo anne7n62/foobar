59a1870805d433af08bff39d9356e7b4f4f8a9f4
Louise <loui59f1@stud.kea.dk>
2021-06-03 20:37:59 +0200

    Popup mobile fixed

--- src/FilterNav.js
+++ src/FilterNav.js
@@ -1,15 +1,21 @@
 function FilterNav({ products, setCategory, btnCategories }) {
+
   function updateCategory(e) {
     setCategory(e.target.innerText);
   }
 
+  console.log(btnCategories)
+
   // Creating an array with the categories
   const categories = [];
+
   btnCategories.forEach((beer) => {
     if (categories.indexOf(beer.category) < 0) {
       categories.push(beer.category);
     }
   });
+
+  console.log(categories)
 
   // Creating a button for each category
   const filterbtns = categories.map((category) => <Filterbtn className="filterbtn" products={products} updateCategory={updateCategory} key={category} category={category}></Filterbtn>);
